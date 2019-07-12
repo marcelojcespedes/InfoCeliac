9944473cedca88edf89661311cab79a87ce67b5e
marcelojcespedes <marcelojcespedes@gmail.com>
2019-07-12 11:58:58 -0300

    das

---
+++ mapa/script.js
@@ -0,0 +1,11 @@
+function iniciarMap(){
+    var coord = {lat:-26.1775303, lng:-58.1781387};
+    var map = new google.maps.Map(document.getElementById('map'),{
+      zoom: 10,
+      center: coord
+    });
+    var marker = new google.maps.Marker({
+      position: coord,
+      map: map
+    });
+}
