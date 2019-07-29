5dc0e9b006966f8159e2d5ab0f86b2fb55f41b40
marcelojcespedes <marcelojcespedes@gmail.com>
2019-07-29 11:38:22 -0300

    dsa

--- conexion/registrar.php
+++ conexion/registrar.php
@@ -1,14 +1,29 @@
 <?php
-include "conexion.php";
+include 'conexion.php';
+
 $nombre = $_POST["nombre"];
 $apellido = $_POST["apellido"];
 $correo = $_POST["correo"];
 $usuario = $_POST["usuario"];
-$contraseña = $_POST["contraseña"];
-$telefono = $_POST["telefono"];
-$fechadenacimiento = $_POST["fechadenacimiento"];
+$password = $_POST["contraseña"];
 
-$insertar = "INSERT INTO usuarios(nombre, apellido, correo, usuario, contraseña, telefono, fechadenacimiento) VALUES ('$nombre', '$apellido', '$correo', '$usuario', '$contraseña', '$telefono', '$fechadenacimiento')";
+$insertar = "INSERT INTO usuarios (nombre, apellido, correo, usuario, contraseña) VALUES ('$nombre', '$apellido', '$correo', '$usuario', '$password')";
+
+$verificar_usuario = mysqli_query($conexion, "SELEC * FROM usuarios WHERE usuario = '$usuario'");
+if (mysqli_num_rows($verificar_usuario)) > 0)}
+echo '<script>
+       alert("el usuario ya esta registrado");
+       window.history.go(-);
+       </script>';
+       exit;
+}
+
+$verificar_correo = mysqli_query($conexion, "SELEC * FROM usuarios WHERE usuario = '$usuario'");
+if (mysqli_num_rows($verificar_usuario) > 0)}
+echo ' El correo ya esta registrado';
+exit;
+}
+
 
 //ejecutar
 
@@ -19,5 +34,6 @@ if (!$resultado) {
     echo 'Registro correcto';
 }
 //cerrar conexion
+
 mysqli_close($conexion); 
 
